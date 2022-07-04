ac55f2cab39b2a6960ff07809b73f97105f75752
Yan Arcanjo <arcanjoyan21@gmail.com>
2022-07-04 12:11:17 -0300

    primeirao versao

---
+++ etapas.js
@@ -0,0 +1,51 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos: [
+            {
+                numero: '38111',
+                name: 'Fulano de tal',
+                partido: 'ABC',
+                fotos: [
+                    {url:'download.jpg', legenda:'Vereador'}
+                ]
+            },
+            {
+                numero:'77222',
+                name: 'Beltrano da Silva',
+                partido: 'DEFG',
+                fotos:[
+                    {url: 'caricatura-digital-mascote-candidato-politico-partido.jpg', legenda: 'Vereador'}
+                ]
+            },
+       ]
+    },
+    {
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos: [
+            {    
+                numero: '99',
+                name: 'Ciclano',
+                partido: 'ABC',
+                vice: 'Cic',
+                fotos:[
+                    {url:'D_NQ_NP_681887-MLB31204193233_062019-O.jpg', legenda: 'Prefeito'},
+                    {url:'caricatura-digital-mascote-candidato-politico-caricatura.jpg', legenda:'Vice-Prefeito', small:true }
+                ]
+            
+            },    
+            {
+                numero: '84',
+                name: 'Zulano',
+                partido: 'QWERTY',
+                vice: 'Zul',
+                foto: [
+                    {url:'caricatura-digital-mascote-candidato-politico-partido.jpg',  legenda:'Prefeito'},
+                    {url: 'download.jpg', legenda:'Vice-Prefeito', small: true}
+                ]
+            }      
+        ]
+    }
+]
